096f5779fe274df52fd873403b411360eb6d7e89
Zsvob <svobodazachary@gmail.com>
2021-12-27 17:27:02 -0500

    fetch to openwx

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
-var apiUrl = api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=566806fcb1c14e56b4f2bf67f8115d7f
+
+
+var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=Cleveland,us&APPID=566806fcb1c14e56b4f2bf67f8115d7f"
+// "api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=566806fcb1c14e56b4f2bf67f8115d7f"
+
+fetch(apiUrl).then(function(response) {
+    response.json().then(function (data) {
+        console.log(data)
+    })
+});
+
+// "https://api.github.com/users/Zsvoboda87/repos"z
