f0d5ca49dc09b80558540904b41dd7208abacd67
Zsvob <svobodazachary@gmail.com>
2021-12-27 17:51:35 -0500

    starting logic current day

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,17 @@
 
 
-var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=Cleveland,us&APPID=566806fcb1c14e56b4f2bf67f8115d7f"
-// "api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=566806fcb1c14e56b4f2bf67f8115d7f"
+var currentDayForecast = function() {
+    var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=Cleveland,us&units=imperial&APPID=566806fcb1c14e56b4f2bf67f8115d7f"
 
-fetch(apiUrl).then(function(response) {
-    response.json().then(function (data) {
-        console.log(data)
-    })
-});
+    fetch(apiUrl).then(function (response) {
+        response.json().then(function (data) {
+            console.log(data.weather[0].icon);
+            console.log(data.main.temp);
+            console.log(data.wind.speed);
+            console.log(data.main.humidity);
 
-// "https://api.github.com/users/Zsvoboda87/repos"z
+        })
+    });
+};
+
+currentDayForecast();
